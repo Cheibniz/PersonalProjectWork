c00f0dbda608ba90ec56070d331980535cfc8586
chaibo <17373106@buaa.edu.cn>
2020-03-08 22:22:18 +0800

    添加计算圆与圆交点的功能

--- src/Personal.cpp
+++ src/Personal.cpp
@@ -5,8 +5,6 @@
 #include<cmath>
 using namespace std;
 
-constexpr auto N = 6;
-
 class Point {
 public:
 	double pointX;
@@ -67,13 +65,34 @@ public:
 	int GetCrossToCircle(Circle c1);
 	int GetCrossToLine(Line l1);
 	bool operator< (const Circle c1)const {
-		return r < c1.r && center < c1.center;
+		return memcmp(this, &c1, sizeof(c1)) < 0;
 	}
 };
 
 int Circle::GetCrossToCircle(Circle c1)
 {
-	return 0;
+	double a1, b1, r1, a0, b0, r0;
+	a0 = center.pointX;
+	b0 = center.pointY;
+	r0 = r;
+	a1 = c1.center.pointX;
+	b1 = c1.center.pointY;
+	r1 = c1.r;
+	double a12 = a1 * a1, b12 = b1 * b1, r12 = r1 * r1, a02 = a0 * a0, b02 = b0 * b0, r02 = r0 * r0;
+	double delta = (-a02 + 2 * a0 * a1 - a12 - b02 + 2 * b0 * b1 - b12 + r02 + 2 * r0 * r1 + r12) * (a02 - 2 * a0 * a1 + a12 + b02 - 2 * b0 * b1 + b12 - r02 + 2 * r0 * r1 - r12);
+	if (delta < 0)
+	{
+		return 0;
+	}
+	double delta_1 = a0 * b02 - a02 * a1 - a0 * a12 + a0 * b12 + a1 * b02 + a1 * b12 - a0 * r02 + a0 * r12 + a1 * r02 - a1 * r12 + a02 * a0 + a12 * a1 - 2 * a0 * b0 * b1 - 2 * a1 * b0 * b1;
+	double delta_2 = sqrt(delta);
+	double delta_3 = 2 * (a02 - 2 * a0 * a1 + a12 + b02 - 2 * b0 * b1 + b12);
+	double delta_4 = a02 * b0 + a02 * b1 + a12 * b0 + a12 * b1 - b0 * b12 - b02 * b1 - b0 * r02 + b0 * r12 + b1 * r02 - b1 * r12 + b0 * b02 + b1 * b12 - 2 * a0 * a1 * b0 - 2 * a0 * a1 * b1;
+	Point cross1((delta_1 - b0 * delta_2 + b1 * delta_2) / delta_3, (delta_4 + a0 * delta_2 - a1 * delta_2) / delta_3);
+	Point cross2((delta_1 + b0 * delta_2 - b1 * delta_2) / delta_3, (delta_4 - a0 * delta_2 + a1 * delta_2) / delta_3);
+	pointSet.insert(cross1);
+	pointSet.insert(cross2);
+	return 2;
 }
 
 int Circle::GetCrossToLine(Line l1)
@@ -103,8 +122,6 @@ void main(int argc, char** argv)
 {
 	ifstream infile;
 	ofstream outfile;
-	//pointSet.insert(Point(0.7, -0.7));
-	//pointSet.insert(Point(-0.7, 0.7));
 	int n;
 	double x0, x1, y0, y1;
 	char type, c1, d0;
